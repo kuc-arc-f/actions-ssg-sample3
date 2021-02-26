081fcc65ced2b56868b8135bb3a16b8d4ad09585
中島浩二 <knaka0209@gmail.com>
2021-02-26 15:02:30 +0900

    up,0226a4

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 //
 module.exports = {
   assetPrefix: process.env.NODE_ENV === "production" ? "/actions-ssg-sample3" : "",
-  env: {
-    CSRF_SECRET : 'secret1234',
-    MY_SITE_NAME: "CMS Sample",    
-  },
 }
